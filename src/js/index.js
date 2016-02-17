18a6eef71dc83342a3bb7aa83aacb9253f67ab3f
kangxu <kangxu@hengtiansoft.com>
2016-02-17 17:06:44 +0800

    idnex.js modift

--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,8 @@ $(document).ready(function() {
         paddingTop: '50px',
         verticalCentered: false,
         afterLoad: function(anchorLink, index) {
-            if(index == 1){
-                
+            if(index == 3){
+                $('.hied-score-hander').addClass('show-socre');
             }
 
         },
@@ -16,11 +16,12 @@ $(document).ready(function() {
                 $('.navbar').removeClass('nav-bar-first');
                 $('.navbar').addClass('nav-bar-others');
             }
+             if(index == 3){
+                $('.hied-score-hander').removeClass('show-socre');
+            }
             if(nextIndex==1){
                 $('.navbar').addClass('nav-bar-first');
             }
-
-
         },
         afterSlideLoad: function( anchorLink, index, slideAnchor, slideIndex) {
 
